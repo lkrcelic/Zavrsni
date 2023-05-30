c4f970e7e615e2c1bf06aa178b622382bd922214
lkrcelic <lkrcelic@croz.net>
2023-05-30 11:22:14 +0200

    proba prelasak na mui pa sam saveao ovo

--- Gestikulator-app/gestikulator-ui/src/components/AnswerText.js
+++ Gestikulator-app/gestikulator-ui/src/components/AnswerText.js
@@ -1,19 +1,9 @@
 import React from "react";
 
-const AnswerText = (
-  correctAnswerClass,
-  wrongAnswerClass,
-  disabledClass,
-  letterMapping,
-  answerText,
-  index,
-  onSelectAnswer
-) => {
+const AnswerText = ({answerText, answerClass, letterMapping, index, onSelectAnswer}) => {
+
   return (
-    <div
-      className={`answer ${correctAnswerClass} ${wrongAnswerClass} ${disabledClass}`}
-      onClick={() => onSelectAnswer(answerText)}
-    >
+    <div className={`answer ${answerClass}`} onClick={() => onSelectAnswer()}>
       <div className="answer-text-letter">{letterMapping[index]}</div>
       <div className="answer-text">
         <span>{answerText}</span>
